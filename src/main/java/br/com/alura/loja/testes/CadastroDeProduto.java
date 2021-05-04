c263a8dd0a0d12bac481356e09d47765a16fdf24
Marcos Barbosa Evangelista <marcos.evangelista@pampa.compasso>
2021-05-04 08:55:50 -0300

    Treinamento JPA e java puro

---
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -0,0 +1,31 @@
+package br.com.alura.loja.testes;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+
+import br.com.alura.loja.dao.CategoriaDao;
+import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
+import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
+
+public class CadastroDeProduto {
+
+	public static void main(String[] args) {
+		Categoria celulares = new Categoria("CELULARES");
+		
+		Produto celular = new Produto(null, "xiaomi Redmim", new BigDecimal("800"),celulares);
+
+		EntityManager em = JPAUtil.getEntityManager();
+
+		CategoriaDao categoriaDao = new CategoriaDao(em);
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		em.getTransaction().begin();
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celular);
+		em.getTransaction().commit();
+		em.close();
+	}
+
+}
